35725946da6d963a3613bb13d7e636921cc600ec
sapiGila <nuke.army@gmail.com>
2018-01-30 19:27:04 +0700

    Update Retrofit
    - Add retrofit configuration
     - timeout
     - log

--- app/src/main/java/basic/app/id/learningbasicandroid/retrofit/RetrofitAdapter.java
+++ app/src/main/java/basic/app/id/learningbasicandroid/retrofit/RetrofitAdapter.java
@@ -1,5 +1,6 @@
 package basic.app.id.learningbasicandroid.retrofit;
 
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,11 +10,27 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitAdapter {
 
+    private static boolean isDebugMode = false;
+
     public Retrofit getRetrofit() {
+        OkHttpClientFactory okHttpClientFactory = OkHttpClientFactory.getInstance();
+        HttpLoggingInterceptor logging = getLoggingLevel(isDebugMode);
+        okHttpClientFactory.getOkHttpClientForRestAdapter().addInterceptor(logging);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://dev.nostratech.com:10093/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClientFactory.getOkHttpClientForRestAdapter().build())
                 .build();
         return retrofit;
+    }
+
+    private static HttpLoggingInterceptor getLoggingLevel(boolean isDebugMode) {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        if (isDebugMode) {
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        } else {
+            logging.setLevel(HttpLoggingInterceptor.Level.NONE);
+        }
+        return logging;
     }
 }
